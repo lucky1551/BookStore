aafce65bd429a6428eb74cd70037952e91aaa9e8
lucky1551 <luckybagdi1111@gmail.com>
2022-07-01 11:11:07 +0530

    hii

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    // "mongodb+srv://admin:nUd0zMDtbX35sTL0@cluster0.5yvzr.mongodb.net/bookStore?retryWrites=true&w=majority"
+    "mongodb+srv://mehul:mehul123@cluster0.xvryk.mongodb.net/BookStore?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000,()=>{
+      console.log('Server is listening');
+    });
+  })
+  .catch((err) => console.log(err));
+
+
